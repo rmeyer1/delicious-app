390b97a96ef5a97a7ca99633e5bac4e49f7f35ba
Rob Meyer <rmeyer@vantagepointlogistics.com>
2020-04-28 22:51:17 -0400

    Added Heart Store feature.

--- dang-thats-delicious/public/javascripts/delicious-app.js
+++ dang-thats-delicious/public/javascripts/delicious-app.js
@@ -4,9 +4,13 @@ import { $, $$ } from './modules/bling';
 import autocomplete from './modules/autocomplete';
 import typeAhead from './modules/typeAhead';
 import makeMap from './modules/map';
+import ajaxHeart from './modules/hearts';
 
 autocomplete( $('#address'), $('#lat'), $('#lng') );
 
 typeAhead( $('.search') );
 
 makeMap( $('#map') );
+
+const heartForms = $$('form.heart');
+heartForms.on('submit', ajaxHeart);
